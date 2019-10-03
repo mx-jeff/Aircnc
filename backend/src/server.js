d4f9afce8369a232cf86eff2d8e26e2e43945aa2
Jeferson <mx.jeferson.12@gmail.com>
2019-10-03 10:46:00 -0300

    frontend and backend adjusts

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
 
 const port = 3333
 const dbPassword = "Aircnc"
@@ -17,7 +19,9 @@ if(mongoose.connect){
     console.log('Banco conectado!')
 }
 
+app.use(cors(/*{ origin: http://localhost:3333 }*/))
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..','images')))
 app.use(routes)
 console.log(`Listening on port ${port}`)
 
